6e3a24595a55256d2ce2d247c39acf7a49b4e565
Nileshmechlin <nilesh.vijay@mechlintech.com>
2025-09-12 19:24:42 +0530

    migration: add new schema for business videos with category and status updates

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import adminRouter from './admin.routes';
+import businessVideoRouter from './businessvideo.routes';
 import categoryRouter from './category.routes';
 import followRouter from './follow.routes';
 import locationRouter from './location.routes';
@@ -15,6 +16,7 @@ const mainRouter = Router();
 
 mainRouter.use('/', welcomeRouter);
 mainRouter.use('/admin', adminRouter);
+mainRouter.use('/business-videos', businessVideoRouter);
 mainRouter.use('/categories', categoryRouter);
 mainRouter.use('/follow', followRouter);
 mainRouter.use('/users', userRouter);
